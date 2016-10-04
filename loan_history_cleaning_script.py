e1ff18dfe7d434369e2a4a876efc4336b3f4e547
ponmatpg <paul.g.ponmattam@gmail.com>
2016-10-03 17:03:48 -0700

    Update loan_history_cleaning_script.py

--- loan_history_cleaning_script.py
+++ loan_history_cleaning_script.py
@@ -12,6 +12,7 @@ loan_history_ca = pd.read_csv("R:/refm-mfe/absnet1/loan_history_ca.csv")
 lhca_col_names = loan_history_ca.columns.values 
 
 # select for desired columns
+# respectively 'absnetloanfk' 'histstartdate' 'histenddate', 'histdelqstatusmbarr' 'histdelqstatusotsrr'
 lhca_col_names_cleaned = lhca_col_names[[0,6,7, 15,16]]
 loan_history_ca_cleaned = loan_history_ca[lhca_col_names_cleaned]
 
